3899ae6b109b58b4d97fdf202940b434f848a82b
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-09-27 20:37:13 +0200

    add useThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { ThemeProvider } from '@emotion/react';
-import React from 'react';
 import { CssBaseline } from '@mui/material';
-import { theme } from './theme/theme';
 import { useThemeContext } from './theme/ThemeContextProvider';
 import { Navigation } from './component/Navigation';
 
 export const App = () => {
-
-  const { theme } = useThemeContext()
+  const { theme } = useThemeContext();
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Navigation/>
+      <Navigation />
     </ThemeProvider>
   );
 };
