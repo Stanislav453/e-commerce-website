9c71a4f75908b1fd456edae740b9c7110dc77102
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-09-28 11:27:14 +0200

    add BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import { ThemeContextProvider } from './theme/ThemeContextProvider.tsx';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeContextProvider>
-      <App />
-    </ThemeContextProvider>
+    <BrowserRouter>
+      <ThemeContextProvider>
+        <App />
+      </ThemeContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
